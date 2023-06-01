f938c17a2f6cb3e52466ce698901e96eea1076b8
Shalia Ramiah <shalia.r20@gmail.com>
2023-06-01 13:17:08 +0200

    wpm calculation

--- wpm.java
+++ wpm.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -22,18 +23,24 @@ public class wpm {
             System.out.print(words[random.nextInt(8)] + " ");       
         } 
 
+        double begin = LocalTime.now().toNanoOfDay();
+
         System.out.println();
 
         Scanner scan = new Scanner(System.in);
         String wordsTyped = scan.nextLine();
 
 
-        System.out.println(wordsTyped);
+        double finish = LocalTime.now().toNanoOfDay();
+        double time = finish - begin;
+        double seconds = time / 1000000000.0;
+        int characters = wordsTyped.length();
 
-		
+        //wpm formula
+        //(x characters / 5) / 1 min = y wpm
+        int wpm = (int) ((((double) characters / 5) / seconds)* 60);
 
-        
-
+        System.out.println("your words per minute is" + wpm);
 
 	}
 
